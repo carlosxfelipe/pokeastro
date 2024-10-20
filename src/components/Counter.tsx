995ae6646efa6ce78c2c7dd0bfa313e8f2609e0b
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-10-20 09:17:52 -0300

    solidjs exercise

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,19 @@
-import { createSignal, type Component } from "solid-js";
+import { createSignal, type Component, type JSXElement } from "solid-js";
 
 interface Props {
-  initialValue: number;
+  initialValue?: number;
+  children?: JSX.Element;
 }
 
 export const Counter: Component<Props> = (props) => {
-  const [counter, setCounter] = createSignal(props.initialValue);
+  const [counter, setCounter] = createSignal(props.initialValue ?? 10);
 
   const buttonClass = "bg-blue-500 p-2 mr-2 rounded w-40";
 
   return (
     <>
-      <h1>Contador</h1>
+      {/* <h1>Contador</h1> */}
+      {props.children}
       <h3>Valor: {counter()} </h3>
       <button class={buttonClass} onClick={() => setCounter((prev) => ++prev)}>
         +1
